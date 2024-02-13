caccf5aada5060e95e5f6f46d85e4c0e67cdf07e
Ah RA <ooz43875@gmail.com>
2024-02-13 18:03:15 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.22 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -0,0 +1,8 @@
+def solution(intStrs, k, s, l):
+    answer = []
+    
+    for intStr in intStrs:
+        if int(intStr[s:s+l]) > k:
+            answer.append(int(intStr[s:s+l]))
+    
+    return answer

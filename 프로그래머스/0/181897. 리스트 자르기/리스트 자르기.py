81364d4b5640be0715fd8342a2bb573655cc3c80
Ah RA <ooz43875@gmail.com>
2024-02-14 16:58:33 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,11 @@
+def solution(n, slicer, num_list):
+    a, b, c = slicer
+    
+    if n == 1:
+        return num_list[0:b+1]
+    if n == 2:
+        return num_list[a:]
+    if n == 3:
+        return num_list[a:b+1]    
+    
+    return num_list[a:b+1:c]

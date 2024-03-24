02be816bdebe6d26229263845cfd2033f7995d5f
Ah RA <ooz43875@gmail.com>
2024-03-24 22:30:57 +0900

    [Silver I] Title: 1로 만들기 2, Time: 596 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -2,25 +2,24 @@ import sys
 N = int(sys.stdin.readline())
 
 dp = [0 for _ in range(N+1)]
-history = [0 for _ in range(N+1)]
 answer = []
 
 for i in range(2, N+1):
     dp[i] = dp[i-1] + 1
-    temp = i - 1
-    if i % 3 == 0 and dp[i//3] + 1 < dp[i]:
-        dp[i] = dp[i//3] + 1
-        temp = i // 3
-    if i % 2 == 0 and dp[i//2] + 1 < dp[i]:
-        dp[i] = dp[i//2] + 1
-        temp = i // 2
-    history[i] = temp
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
 
 idx = N    
-while True:
+while idx > 0:
     answer.append(idx)
-    if idx == 1: break
-    idx = history[idx]
+    if idx % 3 == 0 and dp[idx//3] == dp[idx] - 1:
+        idx //= 3
+    elif idx % 2 == 0 and dp[idx//2] == dp[idx] - 1:
+        idx //= 2
+    else:
+        idx -= 1
 
 print(dp[N])
-print(' '.join(map(str, answer)))
+print(' '.join(map(str, reversed(answer[::-1]))))

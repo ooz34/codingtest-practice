cc3cadb3f053f6b60e5e432352499d3316bf57fa
Ah RA <ooz43875@gmail.com>
2024-04-01 01:00:25 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.31 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,7 @@
+import re
+
 def solution(s):   
     eng_to_num = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
-    
-    for k, v in eng_to_num.items():
-        s = s.replace(k, v)
-
+    for eng, num in eng_to_num.items():
+        s = re.sub(eng, num, s)
     return int(s)

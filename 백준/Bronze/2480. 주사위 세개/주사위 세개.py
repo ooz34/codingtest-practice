48cbf607ace25f29f883d7fd58fd994d2690c00d
Ah RA <ooz43875@gmail.com>
2024-04-08 00:03:32 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,11 @@
+import sys
+a,b,c = map(int, sys.stdin.readline().split())
+
+if a == b == c:
+    print(10000 + a*1000)
+elif a==b or a==c:
+    print(1000 + a*100)
+elif b==c:
+    print(1000 + c*100)
+else:
+    print(max(a,b,c)*100)

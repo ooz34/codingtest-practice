22d805735d62388d2caa1cc53a442d55720e06b8
Ah RA <ooz43875@gmail.com>
2024-04-15 00:53:22 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 20.16 ms
+메모리: 10.3 MB, 시간: 1.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 15일 00:45:42
+2024년 04월 15일 00:53:20
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,42 +1,17 @@
-
 def solution(keymap, targets):
-    answer = [0 for _ in range(len(targets))]
-    keymap_set = set()
-    targets_set = set()
+    answer = []
+    hs = {}
+    for k in keymap:
+        for i, ch in enumerate(k):
+            hs[ch] = min(i + 1, hs[ch]) if ch in hs else i + 1
 
-    for keyarr in keymap:
-        keymap_set.update(keyarr)
-    for target in targets:
-        targets_set.update(target)
-    
-    missing_key = targets_set - keymap_set
-    key_press = {k:0 for k in targets_set}
-    
-    for key in targets_set:
-        if key in  missing_key:
-            key_press[key] = -1
-            continue
-        
-        flag = True
-        while flag:
-            flag = False
-            for i in range(100):
-                for keyarr in keymap:
-                    if len(keyarr) < i+1:
-                        continue
-                    if keyarr[i] in key_press and key_press[keyarr[i]] == 0:
-                        key_press[keyarr[i]] = i+1
-                        if all(v != 0 for v in key_press.values()):
-                            flag = False    
-                            break
-                if flag:
-                    break
-        
-    for i, target in enumerate(targets):
-        for char in target:
-            if key_press[char] == -1:
-                answer[i] = -1
+    for i, t in enumerate(targets):
+        ret = 0
+        for ch in t:
+            if ch not in hs:
+                ret = - 1
                 break
-            answer[i] += key_press[char]
+            ret += hs[ch]
+        answer.append(ret)
 
     return answer

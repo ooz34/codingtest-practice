fc1fa00abfa57fb8b4559fdad8ba49d5a29bd8a1
Ah RA <ooz43875@gmail.com>
2024-04-15 23:05:20 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,3 @@
 import sys
-
 A, B, C = map(int, sys.stdin.readline().split())
-    
-def sol(A, B, C):
-    if B == 1:
-        return A % C
-    temp = sol(A, B//2, C)
-    if B % 2 == 0:
-        return temp * temp % C
-    return (temp * temp % C) * A % C
-
-print(sol(A, B, C))
+print(pow(A, B, C))

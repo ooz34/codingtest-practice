f3cea55e98aca84110ca2ed5943f02786042249b
Ah RA <ooz43875@gmail.com>
2024-04-27 09:02:40 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,7 +1,22 @@
 import sys
-from itertools import combinations
-n, m = map(int, sys.stdin.readline().split())
-nlist = sorted(map(int, sys.stdin.readline().split()))
 
-for p in sorted(set(combinations(nlist, m))):
-    print(*p)
+n, m = map(int, sys.stdin.readline().split())
+nlist = sorted(list(map(int, sys.stdin.readline().split())))
+arr = []
+visited = [False for _ in range(n)]
+
+def backtrack(start):
+    if len(arr) == m:
+        print(*arr)
+        return
+    curr = 0
+    for i in range(start, n):
+        if not visited[i] and curr != nlist[i]:
+            arr.append(nlist[i])
+            visited[i] = True
+            curr = nlist[i]
+            backtrack(i)
+            arr.pop()
+            visited[i] = False
+
+backtrack(0)

a41797f52de3be2161b0533356351acb8e9d59e0
Ah RA <ooz43875@gmail.com>
2024-04-29 20:32:58 +0900

    [Silver II] Title: 부분수열의 합, Time: 252 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,13 +1,18 @@
 import sys
-from itertools import combinations
-
 n, s = map(int, sys.stdin.readline().split())
-seq = list(map(int, sys.stdin.readline().split()))
+seq = list(map(int,sys.stdin.readline().split()))
 cnt = 0
 
-for i in range(1, n + 1):
-    for subseq in combinations(seq, i):
-        if sum(subseq) == s:
+def backtrack(idx, seq_sum):
+    global cnt
+    if idx == n:
+        if seq_sum == s:
             cnt += 1
-
+        return
+    
+    backtrack(idx + 1, seq_sum + seq[idx])
+    backtrack(idx + 1, seq_sum)
+        
+backtrack(0, 0)
+if s==0: cnt -= 1
 print(cnt)

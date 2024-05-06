61a5ec384b96191738449089e0e5822438160c27
Ah RA <ooz43875@gmail.com>
2024-05-06 20:59:12 +0900

    [Silver IV] Title: 수 찾기, Time: 256 ms, Memory: 49460 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import bisect_left
 
 n = int(sys.stdin.readline().strip())
 arr = sorted(map(int, sys.stdin.readline().split()))
@@ -6,19 +7,8 @@ m = int(sys.stdin.readline().strip())
 target = list(map(int, sys.stdin.readline().split()))
 
 for num in target:
-    st = 0
-    en = n-1
-    found = False
-    while st <= en:
-        mid = (st+en)//2
-        val = arr[mid]
-        if val == num:
-            print(1)
-            found = True
-            break
-        elif val < num:
-            st = mid + 1
-        else:
-            en = mid - 1
-    if not found:
-        print(0)
+	idx = bisect_left(arr, num)
+	if idx < n and arr[idx] == num:
+		print(1)
+	else:
+		print(0)

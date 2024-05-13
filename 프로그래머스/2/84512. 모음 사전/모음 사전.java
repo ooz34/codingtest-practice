7416e4b5cf7ecb9f7d3954371e4f2f6f07fb6680
Ah RA Yi <ooz43875@gmail.com>
2024-05-13 16:37:20 +0900

    [level 2] Title: 모음 사전, Time: 3.96 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,23 +1,27 @@
 class Solution {
-    private static int answer = 0;
-    private static int cnt = 0;
+    static int answer = 0;
+    static int cnt = 0;
     
     public int solution(String word) {
         String[] vowels = {"A", "E", "I", "O", "U"};
         dfs(vowels, word, "");
-        return answer-1;
+        return answer;
     }
     
     public static void dfs(String[] vowels, String word, String str){
-        cnt++;
         if (str.equals(word)){
             answer = cnt;
+            return;
         }
         if (str.length() == 5 || answer != 0){
             return;
         }
         for (String vowel: vowels) {
+            cnt++;
             dfs(vowels, word, str + vowel);
+            if (answer != 0) {
+                cnt--;
+            }
         }
     }
 }

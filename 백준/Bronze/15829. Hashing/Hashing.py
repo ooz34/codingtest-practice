d4d12e39d2b72e4157005cfad33d99b33910f67d
Ah RA Yi <ooz43875@gmail.com>
2024-05-18 11:31:29 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,5 +2,5 @@ l = int(input())
 string = input()
 answer = 0
 for i, ch in enumerate(string):
-    answer += (ord(ch)-96)*(31**i) % 1234567891
-print(answer)
+    answer += (ord(ch)-96)*pow(31,i)
+print(answer % 1234567891)

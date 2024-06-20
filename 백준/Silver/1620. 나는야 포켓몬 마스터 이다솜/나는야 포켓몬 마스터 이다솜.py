060f725d075ce459d7f7ba85614b4a80a7738f97
Ah RA Yi <ooz43875@gmail.com>
2024-06-21 05:12:56 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 236 ms, Memory: 54332 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,15 +1,14 @@
 import sys
 n, m = map(int, sys.stdin.readline().split())
-names = {}
-idxs = {}
-for i in range(1, n+1):
-    temp = sys.stdin.readline().strip()
-    names[temp] = i
-    idxs[i] = temp
-    
+idx_name = {}
+name_idx = {}
+for i in range(n):
+    name = sys.stdin.readline().rstrip()
+    idx_name[i+1] = name
+    name_idx[name] = i+1
 for _ in range(m):
-    q = sys.stdin.readline().strip()
+    q = sys.stdin.readline().rstrip()
     if q.isdigit():
-        print(idxs[int(q)])
+        print(idx_name[int(q)])
     else:
-        print(names.get(q))
+        print(name_idx[q])

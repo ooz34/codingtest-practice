7f8c90604e20464530553a9144b5a3b5b97a9354
Ah RA Yi <ooz43875@gmail.com>
2024-06-23 22:50:49 +0900

    [Silver II] Title: 지구 온난화, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -0,0 +1,33 @@
+import sys
+r, c = map(int, sys.stdin.readline().split())
+map1 = []
+map2 = []
+for _ in range(r):
+    row = sys.stdin.readline().rstrip()
+    map1.append(list(row))
+    map2.append(list(row))
+minx, miny, maxx, maxy = r-1, c-1, 0, 0
+for i, row in enumerate(map1):
+    for j, col in enumerate(row):
+        xy = [(1,0), (0,1), (-1,0), (0,-1)]
+        cnt = 0
+        if col == 'X':
+            for x, y in xy:
+                dx = i + x
+                dy = j + y
+                if 0<= dx <r and 0<= dy <c:
+                    if map1[dx][dy]=='.':
+                        cnt += 1
+                else:
+                    cnt += 1
+            if cnt >= 3:
+                map2[i][j] = '.'
+            else:
+                minx = min(minx, i)
+                miny = min(miny, j)
+                maxx = max(maxx, i)
+                maxy = max(maxy, j)
+for i in range(minx, maxx+1):
+    for j in range(miny, maxy+1):
+        print(map2[i][j], end='')
+    print()

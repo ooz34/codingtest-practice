8e870ca6417f95f91a7d8a6ef05e0c6048d95e1c
Ah RA Yi <ooz43875@gmail.com>
2024-06-25 05:27:25 +0900

    [Silver II] Title: 유기농 배추, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,33 +1,28 @@
 import sys
 from collections import deque
-t = int(sys.stdin.readline().strip())
-xy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
 def bfs(graph, x, y):
     q = deque()
-    q.append([x, y])
+    q.append((x, y))
     graph[x][y] = 0
     while q:
-        qx, qy = q.popleft()
-        for dx, dy in xy:
-            nx = qx + dx
-            ny = qy + dy
-            if 0 <= nx < m and 0 <= ny <n and graph[nx][ny]:
-                q.append([nx, ny])
-                graph[nx][ny] = 0
-                
-for _ in range(t):
+        nx, ny = q.popleft()
+        for dx, dy in [(1,0), (0, 1), (-1, 0), (0, -1)]:
+            qx, qy = nx + dx, ny + dy
+            if 0 <= qx < m and 0 <= qy <n and graph[qx][qy]:
+                q.append((qx, qy))
+                graph[qx][qy] = 0
+tc = int(sys.stdin.readline().rstrip())
+for _ in range(tc):
     m, n, k = map(int, sys.stdin.readline().split())
     
-    patch = [[0]*n for _ in range(m)]
+    graph = [[0]*n for _ in range(m)]
     for _ in range(k):
         x, y = map(int, sys.stdin.readline().split())
-        patch[x][y] = 1
-    
+        graph[x][y] = 1
     cnt = 0
     for i in range(m):
         for j in range(n):
-            if patch[i][j]:
-                bfs(patch, i, j)
+            if graph[i][j]:
+                bfs(graph, i, j)
                 cnt += 1
     print(cnt)

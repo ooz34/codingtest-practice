3f83f4be9ee0a67ed45bbe1e54341b93ae5537ba
Ah RA Yi <ooz43875@gmail.com>
2024-07-09 05:19:12 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -1,21 +1,19 @@
 import sys
-from bisect import insort_left
 n, m, l = map(int, sys.stdin.readline().split())
 loc = [0] + list(map(int, sys.stdin.readline().split())) + [l]
 loc.sort()
 
-st = 1
-en = l-1
-answer = 0
-while st <= en:
+st, ed = 1, l-1
+ans = 0
+while st <= ed:
     cnt = 0
-    mid = (st + en)//2
+    mid = (st+ed)//2
     for i in range(1, len(loc)):
         if loc[i] - loc[i-1] > mid:
-            cnt += (loc[i] - loc[i-1] -1)//mid
+           cnt += (loc[i] - loc[i-1] -1)//mid
     if cnt > m:
-        st = mid + 1
+        st = mid+1
     else:
-        en = mid - 1
-        answer = mid
-print(answer)
+        ed = mid-1
+        ans = mid
+print(ans)
